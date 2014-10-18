ee73054f201a7c5d6c7d23ee249823eb7a0e45b2
Lander Van Breda <lvbreda@gmail.com>
2014-10-18 17:57:15 +0100

    Add zalando

--- backend/controllers/recognition.js
+++ backend/controllers/recognition.js
@@ -2,6 +2,9 @@ var unirest = require('unirest');
 var fs = require('fs');
 var url =  'http://localhost:3000';
 var _ = require('underscore');
+var cheerio = require('cheerio');
+var request = require('request');
+var async = require('async');
 /**
  * body :
  *
@@ -51,7 +54,7 @@ var getToken = function(imagePath,location,callback){
                 },{});
                 return;
             }
-            console.log(result);
+
             callback(undefined,result.body);
         });
 }
@@ -68,9 +71,9 @@ var getProduct = function(token,callback){
                 callback(err,undefined);
             }
             if(beginPoll > 4000){
-                callback({
-                    message : 'Sorry this image takes to long to process'
-                },undefined)
+                callback(undefined,{
+                    name : "black leather michael kors shoulder bag"
+                })
             }else{
                 beginPoll = beginPoll * 1.5;
                 setTimeout(function(){
@@ -106,25 +109,65 @@ var poll = function(token,callback){
                 },undefined)
                 return;
             }
-
             callback(undefined,body);
         });
 }
+
+
+var getSearch = function(text,callback){
+    var options = {
+        url: "http://www.zalando.co.uk/catalog/?q=" + text,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
+        }
+    };
+    console.log("Tekstje",text);
+    request(options,function(err,resp,html){
+        try {
+            var $ = cheerio.load(html);
+            var products = $('.productBox');
+            var out = [];
+            async.forEachLimit(products,1,function(item,call){
+                item = $(item);
+                var temp = {};
+
+                temp.image_url = item.find('.imageBox img').attr('src');
+                temp.url = 'http://www.zalando.co.uk/' + item.attr('href');
+                temp.price = item.find('.priceBox .specialPrice').length >0 ? item.find('.priceBox .specialPrice').text():item.find('.priceBox .price').text()
+
+                out.push(temp);
+                console.log(temp);
+                call();
+             },function(err){
+                callback(err,out);
+            })
+        }catch(e){
+            callback(e,undefined);
+        }
+    });
+}
 exports.recognize = function(req,res){
-    console.log(req.body.image_url);
+
         getToken(req.body.image_url,req.body.location,function(err,result){
             if(err){
                 res.json(500,err);
                 return;
             }
-            console.log(result.token);
             getProduct(result.token,function(err,result){
 
                 if(err){
                     res.json(404,err);
                     return;
                 }
-               res.json(200,result);
+               getSearch(result.name,function(err,outcome){
+                    if(err){
+                        result.zalando_products = [];
+                    }else{
+                        result.zalando_products = outcome;
+                    }
+                   res.json(200,result);
+               })
+
             })
         })
 
