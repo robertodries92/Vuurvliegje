6adc83c9a01c8a8394340983ff4699fe4e59127d
Roberto Dries <robertodries92@gmail.com>
2014-10-18 15:09:48 +0100

    app init

---
+++ App/vuurvliegje/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  vuurvliegje
+//
+//  Created by Roberto Dries on 18/10/14.
+//  Copyright (c) 2014 LAROB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
